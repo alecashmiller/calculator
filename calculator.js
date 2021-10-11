c6c1d73aae6f9e2b8ea1c89be1b2f48f84e0fae3
Alec Miller <alecashmiller@gmail.com>
2021-10-11 17:20:23 -0400

    add everything

--- calculator.js
+++ calculator.js
@@ -1,31 +1,119 @@
-function add(a,b) {
-    console.log(a+b);
+let displayValue = '';
+let display = document.querySelector('#display');
+let buttons = document.querySelectorAll('button');
+let ACbutton = document.querySelector('#ACbutton');
+let operatorButtons = document.querySelectorAll('.operator');
+let digitButtons = document.querySelectorAll('.digit')
+let equalButton = document.querySelector('#equals');
+
+
+function add(num1,num2) {
+    result = parseInt(num1)+parseInt(num2);
 }
-function subtract(a,b) {
-    console.log(a-b);
+function subtract(num1,num2) {
+    result = parseInt(num1)-parseInt(num2);
 }
-function multiply(a,b) {
-    console.log(a*b);
+function multiply(num1,num2) {
+    result = (num1*num2);
 }
-function divide(a,b) {
-    if(b == 0) {
+function divide(num1,num2) {
+    if(num2 == 0) {
         console.log('ERROR, do not divide by 0');
     } else {
-        console.log(a/b);
+        result = (num1/num2);
     }
 }
 
 
-function operate(a, operator, b) {
+function operate(num1, operator, num2) {
     if (operator == '+') {
-        add(a,b);
+        add(num1, num2);
     } else if(operator == '-') {
-        subtract(a,b);
+        subtract(num1, num2);
     } else if(operator == '*') {
-        multiply(a,b);
-    } else {
-        divide(a,b);
+        multiply(num1, num2);
+    } else if(operator == '÷') {
+        divide(num1, num2);
     }
+    display.textContent = String(result);
 }
+
+
+let num1 = '';
+let num2 = '';
+let operator = null;
+
+
+function calculateTotal() {
+digitButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if(operator == null) {
+        num1 += button.textContent;
+        display.textContent += button.textContent;
+    }  else {
+        num2 += button.textContent;
+        display.textContent = '';
+        display.textContent += num2;
+    }
+    })
+})
+
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        operator = button.textContent;
+        
+    })
+})
+
+equalButton.addEventListener('click', () => {
+    operate(num1, operator, num2);
+    
+    num1 = result;
+    num2 = ''
+    operator = null;
+    
+})
+
+
+
+
+
+
+
+
+
+// digitButtons.forEach(button => {
+//     button.addEventListener('click', () => {
+//         displayValue += button.textContent;
+//         display.textContent = displayValue;
+//     })
+// })
+
+ACbutton.addEventListener('click', () => {
+    displayValue = '';
+    display.textContent = '';
+    num1 = '';
+    num2 = '';
+    
+})
+
+}
+
+calculateTotal();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
